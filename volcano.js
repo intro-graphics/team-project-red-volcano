e13926aa5e16235119fb276fe9dbd294eae56051
Irfan Sattar <23389884+irfansattar@users.noreply.github.com>
2020-06-02 22:32:02 -0700

    Integrated Ahmed's code with main code

--- volcano.js
+++ volcano.js
@@ -132,13 +132,44 @@ export class Volcano extends Volcano_Base {
         super();
 
         // Load the object model file:
-        this.shapes = {"volcano": new Shape_From_File("assets/volcano_main.obj")};
+        this.shapes = {"volcano": new Shape_From_File("assets/volcano_main.obj"),
+            "water": new Shape_From_File("assets/water.obj"),
+            "ring": new Shape_From_File("assets/ring.obj"),
+            "tree": new Shape_From_File("assets/MapleTreeStem.obj"),
+            "tree_leaves": new Shape_From_File( "assets/MapleTreeLeaves.obj" ),
+            "wall": new Shape_From_File( "assets/wall.obj" )};
 
         // Define material for volcano
         this.volcano = new Material(new defs.Phong_Shader(1), {
             color: color(61 / 255, 51 / 255, 50 / 255, 1),
             ambient: 1,
-            diffusivity: 0
+            diffusivity: 1
+        });
+        this.water = new Material(new defs.Phong_Shader(1), {
+            color: color(0, 0.3, 0.4, 1),
+            ambient: 1,
+            diffusivity: 1
+        });
+        this.ring = new Material(new defs.Phong_Shader(1), {
+            color: color(0.24, 0.1, 0.1, 1),
+            ambient: 1,
+            diffusivity: 1
+        });
+        this.tree = new Material(new defs.Phong_Shader(1), {
+            color: color(61 / 255, 51 / 255, 50 / 255, 1),
+            ambient: 1,
+            diffusivity: 1
+        });
+        this.tree_leaves = new Material(new defs.Phong_Shader(1), {
+            color: color(0, 0.3, 0, 1),
+            ambient: 1,
+            diffusivity: 1
+        });
+        this.wall = new Material(new defs.Phong_Shader(1), {
+            color: color(0, 0.6, 1, 1),
+            ambient: 0.3,
+            diffusivity: 1 ,
+            specularity: 0
         });
     }
 
@@ -154,12 +185,33 @@ export class Volcano extends Volcano_Base {
 
         // Scene setup
         program_state.projection_transform = Mat4.perspective(Math.PI / 4, context.width / context.height, 1, 500);
-        program_state.lights = [new Light(Mat4.rotation(t / 500, 1, 0, 0).times(vec4(3, 2, 10, 1)), color(1, .7, .7, 1), 100000),
+        program_state.lights = [new Light(Mat4.rotation(500, 5, 0, 0).times(vec4(3, 2, 10, 1)), color(1, .7, .7, 1), 100000),
             // new Light(Mat4.translation(0, 10, 0).times(vec4(3, 2, 10, 1)), color(1, .7, .7, 1), 100000)
         ];
 
         // Draw volcano
         const model_transform = Mat4.identity();
         this.shapes.volcano.draw(context, program_state, model_transform, this.volcano);
+
+        const model_transform2 = Mat4.scale(2, 2, 2).times(Mat4.translation(0, -0.35, 0));
+        this.shapes.water.draw(context, program_state, model_transform2, this.water);
+
+        const model_transform3 = Mat4.scale(2.2, 2.2, 2.2).times(Mat4.translation(0, -0.35, 0));
+        this.shapes.ring.draw(context, program_state, model_transform3, this.ring);
+
+        const model_transform4 = Mat4.scale(0.06, 0.06, 0.06).times(Mat4.translation(-16, -2.5, -25));
+        this.shapes.tree.draw(context, program_state, model_transform4, this.tree);
+        const model_transform6 = Mat4.scale(0.06, 0.06, 0.06).times(Mat4.translation(-18, -2.7, -25));
+        this.shapes.tree.draw(context, program_state, model_transform6, this.tree);
+
+        const model_transform5 = Mat4.scale(0.06, 0.06, 0.06).times(Mat4.translation(-16, -2.5, -25));
+        this.shapes.tree_leaves.draw(context, program_state, model_transform5, this.tree_leaves);
+        const model_transform7 = Mat4.scale(0.06, 0.06, 0.06).times(Mat4.translation(-18, -2.7, -25));
+        this.shapes.tree_leaves.draw(context, program_state, model_transform7, this.tree_leaves);
+
+
+
+        const model_transform8 = Mat4.scale(5, 5, 2.2).times(Mat4.translation(1, -0.35, 10).times( Mat4.rotation( 0.56,0,1,0 ) ));
+        this.shapes.wall.draw(context, program_state, model_transform8, this.wall);
     }
 }
