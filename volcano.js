8227434bd86981de4f598f0ffa173e2a9c99b92d
Irfan Sattar <23389884+irfansattar@users.noreply.github.com>
2020-06-05 16:33:20 -0700

    Code clean-up and organizational refactor

--- volcano.js
+++ volcano.js
@@ -103,57 +103,6 @@ export class Shape_From_File extends Shape {                                   /
     }
 }
 
-// Sphere shape from assignment 3
-window.Subdivision_Sphere =
-    class Subdivision_Sphere extends Shape  // This Shape defines a Sphere surface, with nice uniform triangles.  A subdivision surface (see
-    {                                       // Wikipedia article on those) is initially simple, then builds itself into a more and more
-                                            // detailed shape of the same layout.  Each act of subdivision makes it a better approximation of
-                                            // some desired mathematical surface by projecting each new point onto that surface's known
-                                            // implicit equation.  For a sphere, we begin with a closed 3-simplex (a tetrahedron).  For each
-                                            // face, connect the midpoints of each edge together to make more faces.  Repeat recursively until
-                                            // the desired level of detail is obtained.  Project all new vertices to unit vectors (onto the
-        constructor(max_subdivisions)       // unit sphere) and group them into triangles by following the predictable pattern of the recursion.
-        {
-            super("positions", "normals", "texture_coords");                      // Start from the following equilateral tetrahedron:
-            this.positions.push(...vec.cast([0, 0, -1], [0, .9428, .3333], [-.8165, -.4714, .3333], [.8165, -.4714, .3333]));
-
-            this.subdivideTriangle(0, 1, 2, max_subdivisions);  // Begin recursion.
-            this.subdivideTriangle(3, 2, 1, max_subdivisions);
-            this.subdivideTriangle(1, 0, 3, max_subdivisions);
-            this.subdivideTriangle(0, 2, 3, max_subdivisions);
-
-            for (let p of this.positions) {
-                this.normals.push(p.copy());                 // Each point has a normal vector that simply goes to the point from the origin.
-
-                // Textures are tricky.  A Subdivision sphere has no straight seams to which image
-                // edges in UV space can be mapped.  The only way to avoid artifacts is to smoothly
-                this.texture_coords.push(                      // wrap & unwrap the image in reverse - displaying the texture twice on the sphere.
-                    vec.of(Math.asin(p[0] / Math.PI) + .5, Math.asin(p[1] / Math.PI) + .5))
-            }
-        }
-
-        subdivideTriangle(a, b, c, count)   // Recurse through each level of detail by splitting triangle (a,b,c) into four smaller ones.
-        {
-            if (count <= 0) {
-                this.indices.push(a, b, c);
-                return;
-            }  // Base case of recursion - we've hit the finest level of detail we want.
-
-            var ab_vert = this.positions[a].mix(this.positions[b], 0.5).normalized(),     // We're not at the base case.  So, build 3 new
-                ac_vert = this.positions[a].mix(this.positions[c], 0.5).normalized(),     // vertices at midpoints, and extrude them out to
-                bc_vert = this.positions[b].mix(this.positions[c], 0.5).normalized();     // touch the unit sphere (length 1).
-
-            var ab = this.positions.push(ab_vert) - 1,      // Here, push() returns the indices of the three new vertices (plus one).
-                ac = this.positions.push(ac_vert) - 1,
-                bc = this.positions.push(bc_vert) - 1;
-
-            this.subdivideTriangle(a, ab, ac, count - 1);          // Recurse on four smaller triangles, and we're done.  Skipping every
-            this.subdivideTriangle(ab, b, bc, count - 1);          // fourth vertex index in our list takes you down one level of detail,
-            this.subdivideTriangle(ac, bc, c, count - 1);          // and so on, due to the way we're building it.
-            this.subdivideTriangle(ab, bc, ac, count - 1);
-        }
-    }
-
 // Use to organize adding any necessary controls
 class Volcano_Base extends Scene {
     constructor() {
@@ -178,31 +127,13 @@ class Volcano_Base extends Scene {
     }
 }
 
-
 function getRndInteger(min, max) {
-    return Math.floor(Math.random() * (max - min) ) + min;
+    return Math.floor(Math.random() * (max - min)) + min;
 }
 
 function getRandomArbitrary(min, max) {
     return Math.random() * (max - min) + min;
 }
-
-
-let particles = [];
-let velocity = [];
-let particles2 = [];
-let velocity2 = [];
-let xDrop = 0;
-let yDrop = 0;
-let rainOn = 0;
-let smokeOn = 0;
-let cyCloude = 8;
-let cxCloude = 5;
-let lava2Z = 0;
-let lava3Z = 0;
-let lava4Z = 0;
-let lava5Z = 0;
-let lava6Z = 0;
 
 export class Volcano extends Volcano_Base {
     constructor() {
@@ -217,12 +148,11 @@ export class Volcano extends Volcano_Base {
             "medieval_house": new Shape_From_File("assets/medieval_simple_house.obj"),
             "shack": new Shape_From_File("assets/shack.obj"),
             "watchtower": new Shape_From_File("assets/watchtower.obj"),
-            "smoke":new Shape_From_File("assets/smoke.obj"),
-            "cloud":new Shape_From_File("assets/cloud.obj"),
-            "drop":new Shape_From_File("assets/drop.obj"),
-            "lava2":new Shape_From_File("assets/lava2.obj"),
-            "lava3":new Shape_From_File("assets/lava3.obj"),
-            // "lava": new (Subdivision_Sphere.prototype.make_flat_shaded_version())(1)
+            "smoke": new Shape_From_File("assets/smoke.obj"),
+            "cloud": new Shape_From_File("assets/cloud.obj"),
+            "drop": new Shape_From_File("assets/drop.obj"),
+            "lava2": new Shape_From_File("assets/lava2.obj"),
+            "lava3": new Shape_From_File("assets/lava3.obj")
         };
 
         // Define material for volcano
@@ -303,81 +233,87 @@ export class Volcano extends Volcano_Base {
             diffusivity: 0.2
         });
 
-        for(let j = 0; j < 130; j++){
-            velocity[j] = getRandomArbitrary(0.2, 1) ;
+        this.particles = [];
+        this.velocity = [];
+        this.particles2 = [];
+        this.velocity2 = [];
+        this.xDrop = 0;
+        this.yDrop = 0;
+        this.rainOn = 0;
+        this.smokeOn = 1;
+        this.cyCloude = 8;
+        this.cxCloude = 5;
+        this.lava2Z = 0;
+        this.lava3Z = 0;
+        this.lava4Z = 0;
+        this.lava5Z = 0;
+        this.lava6Z = 0;
+
+        for (let j = 0; j < 130; j++) {
+            this.velocity[j] = getRandomArbitrary(0.2, 1);
         }
 
-        for(let j = 0; j < 50; j++){
-            velocity2[j] = getRandomArbitrary(0.1, 0.2) ;
+        for (let j = 0; j < 50; j++) {
+            this.velocity2[j] = getRandomArbitrary(0.1, 0.2);
         }
-        this.crosshair_Matrix = Mat4.scale(0.25,0.25,0.25).times(Mat4.translation(5, 8, 1.1));
+        this.crosshair_Matrix = Mat4.scale(0.25, 0.25, 0.25).times(Mat4.translation(5, 8, 1.1));
 
         this.background_toggle = this.background;
     }
 
-    make_control_panel()
-    {
-        this.key_triggered_button( "Graduation Background on/off", [ "b" ], () => this.background_toggle = this.background_toggle === this.background ? this.video_background : this.background);
+    make_control_panel() {
+        this.key_triggered_button("Graduation Background on/off", ["b"], () => this.background_toggle = this.background_toggle === this.background ? this.video_background : this.background);
         this.new_line();
-        this.key_triggered_button( "Rain on/off", [ "t" ], this.Rain );
-        this.key_triggered_button( "Smoke on/off", [ "m" ], this.Smoke );
+        this.key_triggered_button("Rain on/off", ["t"], this.Rain);
+        this.key_triggered_button("Smoke on/off", ["m"], this.Smoke);
         this.new_line();
-        this.key_triggered_button( "Move Left", [ "j" ], this.move_left );
-        this.key_triggered_button( "Move Right", [ "l" ], this.move_right );
-        this.key_triggered_button( "Move Up", [ "i" ], this.move_up );
-        this.key_triggered_button( "Move Down", [ "k" ], this.move_down );
+        this.key_triggered_button("Move Left", ["j"], this.move_left);
+        this.key_triggered_button("Move Right", ["l"], this.move_right);
+        this.key_triggered_button("Move Up", ["i"], this.move_up);
+        this.key_triggered_button("Move Down", ["k"], this.move_down);
     }
 
-    move_left()
-    {
+    move_left() {
         this.crosshair_Matrix = this.crosshair_Matrix.times(Mat4.translation(0.2, 0, 0));
-        cxCloude +=0.2;
-        xDrop += 2.5;
+        this.cxCloude += 0.2;
+        this.xDrop += 2.5;
     }
-    move_right()
-    {
+
+    move_right() {
         this.crosshair_Matrix = this.crosshair_Matrix.times(Mat4.translation(-0.2, 0, 0));
-        cxCloude -=0.2;
-        xDrop -= 2.5;
+        this.cxCloude -= 0.2;
+        this.xDrop -= 2.5;
     }
 
-    move_up()
-    {
+    move_up() {
         this.crosshair_Matrix = this.crosshair_Matrix.times(Mat4.translation(0, 0.2, 0));
-        cyCloude += 0.2;
-        yDrop += 2.5;
+        this.cyCloude += 0.2;
+        this.yDrop += 2.5;
     }
 
-    move_down()
-    {
+    move_down() {
         this.crosshair_Matrix = this.crosshair_Matrix.times(Mat4.translation(0, -0.2, 0));
-        cyCloude -= 0.2;
-        yDrop -= 2.5;
+        this.cyCloude -= 0.2;
+        this.yDrop -= 2.5;
     }
 
-    Rain()
-    {
-        if(rainOn === 0){
-            rainOn = 1
-        }
-        else{
-            rainOn = 0;
-        }
-    }
-
-    Smoke()
-    {
-        if(smokeOn === 0){
-            smokeOn = 1
-        }
-        else{
-            smokeOn = 0;
+    Rain() {
+        if (this.rainOn === 0) {
+            this.rainOn = 1
+        } else {
+            this.rainOn = 0;
         }
     }
 
+    Smoke() {
+        if (this.smokeOn === 0) {
+            this.smokeOn = 1
+        } else {
+            this.smokeOn = 0;
+        }
+    }
 
     //######################  Display  ############################\\
-
 
     display(context, program_state) {
         super.display(context, program_state);
@@ -412,120 +348,100 @@ export class Volcano extends Volcano_Base {
             .times(Mat4.scale(0.8, 0.8, 0.8));
         this.shapes.volcano.draw(context, program_state, volcano_transform, this.volcano);
 
-
-        //Draw Smoke
+        // Draw Smoke
         this.particles.push(this.smoke);
-
-
-
-        //Particles Movement
-        for (let i = 0; i < particles.length; i++){
+        // Smoke particles movement component
+        for (let i = 0; i < this.particles.length; i++) {
             let smoke_transform = Mat4.scale(
-                0.02 + (10 + ((t/((velocity[i])*800))%30))*3/1000,
-                0.02 + (10 + ((t/((velocity[i])*800))%30))*3/1000,
-                0.02 + (10 + ((t/((velocity[i])*800))%30))*3/1000)
-                .times(Mat4.translation(-2 + i/20, ((t/((velocity[i])*800))%30), 4));
-            if(10 + ((t/((velocity[i])*800))%30) < 29){
-                let distance = (cyCloude + 6.5) - (((t/((velocity[i])*800))%30));
-                let range = cyCloude - (-5 + i/15);
-                if(!(cxCloude < 1.8  && cxCloude > -1.4 && distance < 0.06 )){
-                    if(smokeOn === 0){
-                        this.shapes.smoke.draw(context, program_state, smoke_transform, particles[i]);
+                0.02 + (10 + ((t / ((this.velocity[i]) * 800)) % 30)) * 3 / 1000,
+                0.02 + (10 + ((t / ((this.velocity[i]) * 800)) % 30)) * 3 / 1000,
+                0.02 + (10 + ((t / ((this.velocity[i]) * 800)) % 30)) * 3 / 1000)
+                .times(Mat4.translation(-2 + i / 20, ((t / ((this.velocity[i]) * 800)) % 30), 4));
+            if (10 + ((t / ((this.velocity[i]) * 800)) % 30) < 29) {
+                let distance = (this.cyCloude + 6.5) - (((t / ((this.velocity[i]) * 800)) % 30));
+                let range = this.cyCloude - (-5 + i / 15);
+                if (!(this.cxCloude < 1.8 && this.cxCloude > -1.4 && distance < 0.06)) {
+                    if (this.smokeOn === 0) {
+                        this.shapes.smoke.draw(context, program_state, smoke_transform, this.particles[i]);
                     }
                 }
             }
         }
 
-        //Draw Smoke cloud
+        // Draw rain cloud
         this.shapes.cloud.draw(context, program_state, this.crosshair_Matrix, this.cloud);
 
-        //Draw lava
-        let lavaY = -1.3 + t/10000;
-        if (lavaY > 0 ){
-            lavaY = 0;
-        }
-        const lava_transform1 = Mat4.scale(0.25,0.25,0.25).times(Mat4.translation(0, lavaY, 1.1));
-        this.shapes.cloud.draw(context, program_state, lava_transform1, this.lava);
-
-        if(lavaY !== 0){
-            lava2Z = - t/20000;
-        }
-        if (lava2Z < -0.3){
-            lava2Z = -0.3;
-        }
-        const lava_transform2 = Mat4.scale(0.25,0.25,0.25).times(Mat4.translation(0, -1.6, lava2Z));
-        this.shapes.lava2.draw(context, program_state, lava_transform2, this.lava);
-
-        if(lava3Z !== -0.3){
-            lava3Z = - t/20000;
-        }
-        if (lava3Z < -0.5){
-            lava3Z = -0.5;
-        }
-        const lava_transform5 = Mat4.scale(0.20,0.20,0.20).times(Mat4.translation(-0.91, -1.7, lava3Z).times(Mat4.rotation(4.4, 0, 1, 0)));
-        this.shapes.lava3.draw(context, program_state, lava_transform5, this.lava);
-
-
-        if(lava3Z !== -0.5){
-            lava4Z = - t/5000;
-        }
-        if (lava4Z < -2.4){
-            lava4Z = -2.4;
-        }
-        const lava_transform4 = Mat4.scale(0.20,0.20,0.20).times(Mat4.translation(-1.5, -2.7, lava4Z).times(Mat4.rotation(4.4, 0, 1, 0)));
-        this.shapes.lava3.draw(context, program_state, lava_transform4, this.lava);
-
-
-        if(lava4Z !== -2.4){
-            lava5Z = - t/3000;
-        }
-        if (lava5Z < -3.42){
-            lava5Z = -3.42;
-        }
-        const lava_transform6 = Mat4.scale(0.20,0.20,0.20).times(Mat4.translation(-1.5, -3.2, lava5Z).times(Mat4.rotation(4.4, 0, 1, 0)));
-        this.shapes.lava3.draw(context, program_state, lava_transform6, this.lava);
-
-
-
-
-        if(lava5Z !== -3.42){
-            lava6Z = - t/2500;
-        }
-        if (lava6Z < -5){
-            lava6Z = -5;
-        }
-        const lava_transform3 = Mat4.scale(0.25,0.241,0.25).times(Mat4.translation(-0.9, -3.1, lava6Z).times(Mat4.rotation(3.7, 1, 0, 1)));
-        this.shapes.lava2.draw(context, program_state, lava_transform3, this.lava);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        //Draw Drop
-        this.drop = new Material(new defs.Phong_Shader(1), {color: color(0, 0, 1, 1), ambient: .3, diffusivity: 1, specularity: .5 });
-        particles2.push(this.drop);
+        // Draw rain drop
+        this.drop = new Material(new defs.Phong_Shader(1), {
+            color: color(0, 0, 1, 1),
+            ambient: .3,
+            diffusivity: 1,
+            specularity: .5
+        });
+        this.particles2.push(this.drop);
         //Drop particles
-        for (let i = 0; i < particles2.length; i++){
-            let model_transform10 = Mat4.scale(0.02,0.02,0.02)
-                                    .times(Mat4.translation(55 + i/2 + xDrop, (yDrop + 100) - ((t/((velocity2[i])*400))%120), 20));
-            if((150 - ((t/((velocity2[i])*400))%120)) > 5)
-            {
-                if(rainOn === 1){
-                    this.shapes.drop.draw(context, program_state, model_transform10, particles2[i]);
+        for (let i = 0; i < this.particles2.length; i++) {
+            let model_transform10 = Mat4.scale(0.02, 0.02, 0.02)
+                .times(Mat4.translation(55 + i / 2 + this.xDrop, (this.yDrop + 100) - ((t / ((this.velocity2[i]) * 400)) % 120), 20));
+            if ((150 - ((t / ((this.velocity2[i]) * 400)) % 120)) > 5) {
+                if (this.rainOn === 1) {
+                    this.shapes.drop.draw(context, program_state, model_transform10, this.particles2[i]);
                 }
             }
         }
+
+        //Draw lava
+        let lavaY = -1.3 + t / 10000;
+        if (lavaY > 0) {
+            lavaY = 0;
+        }
+        const lava_transform1 = Mat4.scale(0.25, 0.25, 0.25).times(Mat4.translation(0, lavaY, 1.1));
+        this.shapes.cloud.draw(context, program_state, lava_transform1, this.lava);
+
+        if (lavaY !== 0) {
+            this.lava2Z = -t / 20000;
+        }
+        if (this.lava2Z < -0.3) {
+            this.lava2Z = -0.3;
+        }
+        const lava_transform2 = Mat4.scale(0.25, 0.25, 0.25).times(Mat4.translation(0, -1.6, this.lava2Z));
+        this.shapes.lava2.draw(context, program_state, lava_transform2, this.lava);
+
+        if (this.lava3Z !== -0.3) {
+            this.lava3Z = -t / 20000;
+        }
+        if (this.lava3Z < -0.5) {
+            this.lava3Z = -0.5;
+        }
+        const lava_transform5 = Mat4.scale(0.20, 0.20, 0.20).times(Mat4.translation(-0.91, -1.7, this.lava3Z).times(Mat4.rotation(4.4, 0, 1, 0)));
+        this.shapes.lava3.draw(context, program_state, lava_transform5, this.lava);
+
+        if (this.lava3Z !== -0.5) {
+            this.lava4Z = -t / 5000;
+        }
+        if (this.lava4Z < -2.4) {
+            this.lava4Z = -2.4;
+        }
+        const lava_transform4 = Mat4.scale(0.20, 0.20, 0.20).times(Mat4.translation(-1.5, -2.7, this.lava4Z).times(Mat4.rotation(4.4, 0, 1, 0)));
+        this.shapes.lava3.draw(context, program_state, lava_transform4, this.lava);
+
+        if (this.lava4Z !== -2.4) {
+            this.lava5Z = -t / 3000;
+        }
+        if (this.lava5Z < -3.42) {
+            this.lava5Z = -3.42;
+        }
+        const lava_transform6 = Mat4.scale(0.20, 0.20, 0.20).times(Mat4.translation(-1.5, -3.2, this.lava5Z).times(Mat4.rotation(4.4, 0, 1, 0)));
+        this.shapes.lava3.draw(context, program_state, lava_transform6, this.lava);
+
+        if (this.lava5Z !== -3.42) {
+            this.lava6Z = -t / 2500;
+        }
+        if (this.lava6Z < -5) {
+            this.lava6Z = -5;
+        }
+        const lava_transform3 = Mat4.scale(0.25, 0.241, 0.25).times(Mat4.translation(-0.9, -3.1, this.lava6Z).times(Mat4.rotation(3.7, 1, 0, 1)));
+        this.shapes.lava2.draw(context, program_state, lava_transform3, this.lava);
 
         // Draw buildings
         const shack_transform2 = Mat4.identity()
